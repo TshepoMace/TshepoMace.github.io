b971c24a0f0a6564eacb2bc14b50bbea45ee70d4
TshepoMace <45984510+TshepoMace@users.noreply.github.com>
2023-09-29 08:46:21 +0200

    Update scripts.js

--- shift/scripts.js
+++ shift/scripts.js
@@ -1,1 +1,19 @@
+const navbar = document.getElementById("navbar");
+const navLinks = document.querySelectorAll("#navbar ul li a");
 
+window.addEventListener("scroll", () => {
+    if (window.scrollY < 80) { // Adjust the scroll threshold as needed
+        navbar.style.backgroundColor = "transparent"; // Make navbar transparent
+        navbar.style.boxShadow = "none";
+        navLinks.forEach(link => {
+            link.classList.remove("black-text");
+        });
+    } else {
+        navbar.style.backgroundColor = "#fff"; // Restore solid background color
+        navbar.style.boxShadow = "0 4px 8px 0 rgba(0,0,0,0.2)";
+        navLinks.forEach(link => {
+            link.classList.add("black-text"); 
+        });
+
+    }
+});
