72853343f4eaddef44a102975b8535a27642e54a
TshepoMace <45984510+TshepoMace@users.noreply.github.com>
2023-09-29 11:15:15 +0200

    Update scripts.js

--- shift/scripts.js
+++ shift/scripts.js
@@ -17,3 +17,12 @@ window.addEventListener("scroll", () => {
 
     }
 });
+
+
+function toggleMenu() {
+    $("#hamburgerMenu").toggleClass("collapsed");  
+    $(".aa-mobile-overlay").animate({
+          height: "toggle",
+          opacity: "toggle"
+      }, 300);
+  }
